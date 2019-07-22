bb43abb88896e2d37cc408c340a8577ee75801e6
Ty Overby <ty@pre-alpha.com>
2019-07-22 00:54:39 +0000

    start generic dom

---
+++ generic_dom/generic_dom.ml
@@ -0,0 +1,31 @@
+module Node_kind = struct
+  type t =
+    | Text
+    | Element
+end
+
+module type S = sig
+  type node
+
+  module Text : sig
+    type t
+
+    val create_text : string -> t
+    val change_text : t -> string -> unit
+    val text_to_node : t -> node
+    val node_to_text_exn : node -> t
+  end
+
+  module Element : sig
+    type t
+
+    val element_to_node : t -> node
+    val node_to_element_exn : node -> t
+    val create_element : kind:string -> t
+    val add_attribue : t -> key:string -> value:string -> unit
+    val remove_attribue : t -> key:string -> value:string -> unit
+    val insert_before : parent:t -> target:node -> to_insert:node -> unit
+    val append : parent:t -> to_insert:node -> unit
+    val prepend : parent:t -> to_insert:node -> unit
+  end
+end
